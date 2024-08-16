f54130461e1ef43c7f4add903673b16783bba94c
mejibolorunduro <davidbolorunduro2004@gmail.com>
2024-08-16 01:13:27 +0000

    done

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
-"""
+''' rotates a 2D matrix '''
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
+    '''
+        rotates 2d matrix 90 degrees clockwise
+        rotation happens in place
+        args:
+            matrix
+    '''
     rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    columns = rows - 1
+
+    for i in range(0, int(rows / 2)):
+        for j in range(i, columns - i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[columns - j][i]
+            matrix[columns - j][i] = matrix[columns - i][columns - j]
+            matrix[columns - i][columns - j] = matrix[j][columns - i]
+            matrix[j][columns - i] = temp
